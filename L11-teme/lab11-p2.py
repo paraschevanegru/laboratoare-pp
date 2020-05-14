57619db0ab8c231a23d003ebfcae6e603f0d907a
Parascheva Negru <parascheva.negru@gmail.com>
2020-05-14 13:57:01 +0300

    adaugare comentarii

--- L11-teme/lab11-p2.py
+++ L11-teme/lab11-p2.py
@@ -6,30 +6,39 @@ import shlex
 def functie():
     parser = argparse.ArgumentParser(description='Process command')
     parser.add_argument('command', metavar='cmd', nargs='+',
-                        help='an integer for the accumulator')
+                        help='a command in quotes')
     args = parser.parse_args()
-    cmd = args.command[0].split('|')
+    # pune comenzile separat intr-o lista
+    cmd = args.command[0].split('|')  
     print(cmd)
-    print(run(cmd))
+    # executia la comada
+    print(run(cmd))  
 
 
 def run(command_list):
     process_list = list()
     previous_process = None
     for command in command_list:
-        args = shlex.split(command)
+         # shlex.split imparte un string cu acele comenzi intr-o lista
+        args = shlex.split(command) 
         if previous_process is None:
-            process = subprocess.Popen(args, stdout=subprocess.PIPE)
+            # daca nu exista un proces anterior pune in stdout rezultatul executiei comenzii
+            process = subprocess.Popen(args, stdout=subprocess.PIPE)  
         else:
             process = subprocess.Popen(args,
                                         stdin=previous_process.stdout,
                                         stdout=subprocess.PIPE)
+        # creeaza lista de procese                              
         process_list.append(process)
         previous_process = process
+    # ia ultimul proces din lista
     last_process = process_list[-1]
+    # citim output-ul de la acest proces si ignoram erorile
     output, _ = last_process.communicate()
+    # facem decode la byte output
     return output.decode("utf-8")
 
 if __name__ == '__main__':
+    # un exemplu de rulare in linia de comanda ar fi: python lab11-p2.py "ls -la | grep lab11"
     functie()
     
